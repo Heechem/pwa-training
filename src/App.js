e9e9bedf770b7dfe94dce1268ae77e189a8d52b4
Heechem <hichempeter@gmail.com>
2023-05-19 07:32:30 -0600

    Adding UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import shuffle from './utilities/shuffle';
+import Card from './components/Card';
 
 function App() {
   const [cards, setCards] = useState(shuffle);
 
   return (
     <div className='App'>
-      <div className='grid'></div>
+      <div className='grid'>
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+          return <Card key={id} image={image} selected={false} />;
+        })}
+      </div>
     </div>
   );
 }
